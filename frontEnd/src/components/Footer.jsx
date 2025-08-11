ed88c088de1f3d7f102da972b82211198587f613
SARABJIT SHARMA <sarabjitsharma88@gmail.com>
2025-08-11 10:43:57 +0530

    first commit

---
+++ frontEnd/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="footer-content">
+                    <div className="footer-links">
+                        <a href="#">About MLU</a>
+                        <a href="#">GitHub</a>
+                        <a href="#">Contact</a>
+                        <a href="#">Privacy Policy</a>
+                    </div>
+                    <p>&copy; 2025 Machine Learning University - Amazon Web Services</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
