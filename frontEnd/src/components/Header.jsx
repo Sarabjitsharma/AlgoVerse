6de92a216822a21f9fdc6da182355ac3ba239857
Tanish <tanishgargchd@gmail.com>
2025-09-10 22:14:51 +0530

    Authorization Page

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
             <Link to="/auth">
               <button type="button"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Get started
+                Login/Signup
               </button>
             </Link>
             <button data-collapse-toggle="navbar-sticky" type="button"
